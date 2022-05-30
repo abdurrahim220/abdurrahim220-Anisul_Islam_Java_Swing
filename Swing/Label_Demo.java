0e9cd6cf1897dfc9dc0f9506fca168fbecaea0db
abdurrahim220 <z.abdurrahim5@gmail.com>
2022-05-30 07:48:58 +0600

    FirstUpdate

--- Swing/Label_Demo.java
+++ Swing/Label_Demo.java
@@ -7,6 +7,8 @@ public class Label_Demo extends JFrame {
     private Container c;
     private JLabel userLabel1,passlabel;
     private Font f;
+    private JLabel ImgLable;
+    private ImageIcon img;
 
     Label_Demo(){
         initComponetns();
@@ -19,16 +21,31 @@ public class Label_Demo extends JFrame {
 
         f = new Font("Arial",Font.BOLD,14);
 
+        img =new ImageIcon(getClass().getResource("Private-Label-Icon.png"));
+        ImgLable = new JLabel(img);
+        ImgLable.setBounds(30 ,130,img.getIconWidth(),img.getIconHeight());
+        c.add(ImgLable);
+
 
         userLabel1 =new JLabel();
         userLabel1.setText("Enter Your User Nmae : ");
         userLabel1.setBounds(50,20,200,50);
         userLabel1.setFont(f);
+        userLabel1.setForeground(Color.red);
+        userLabel1.setBackground(Color.yellow);
+        userLabel1.setToolTipText("Enter your user name");
+        userLabel1.setOpaque(true);
         c.add(userLabel1);
+
+        //System.out.println(" "+userLabel1.getText());
+        String s = userLabel1.getToolTipText();
+        System.out.println(" "+s);
 
         passlabel = new JLabel("Enter your Password");
         passlabel.setBounds(50,70,200,50);
         passlabel.setFont(f);
+        passlabel.setForeground(Color.MAGENTA);
+        passlabel.setOpaque(true);
         c.add(passlabel);
     }
     public static void main(String[] args) {
